259bec86bd1c575a79f47fae82d310bc0a45ca96
Apostolis Stamatis <apostolis.stamatis1@gmail.com>
2023-11-22 00:38:44 +0200

    Adds tests

--- tests/test_syntax.sh
+++ tests/test_syntax.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # for FILE in /home/apostolis/Shmmy/compilers/grace/ntua_compilers/grace/syntax_gen/progs/*;
-for FILE in /home/apostolis/Shmmy/compilers/grace/yet-another-grace-compiler/tests/semantic/*;
+for FILE in /home/apostolis/Shmmy/compilers/grace/yet-another-grace-compiler/tests/type_checking/*;
 do 
     if ! ./gracec < $FILE > /dev/null; then echo "$FILE Failed"
     # else echo "Success"
