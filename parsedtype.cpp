79f63588e7a6179042295fe8d65b4688f8893895
Giorgos Kosmas <44445106+gkosm314@users.noreply.github.com>
2023-11-28 20:11:43 +0100

    Fixes bug related to type of intermediate LMatrix types

--- parsedtype.cpp
+++ parsedtype.cpp
@@ -30,18 +30,28 @@ void ParsedType::printAST(std::ostream &out) const {
 
 Type toType(ParsedType *t)
 {
- /* Formula : baseType_(i+1) = typeArray( dim[i] , baseType_(i) )
-  * Example : int[5][12][3]
-  *    initial -> baseType_0                             = int
-  *    iter. 1 -> baseType_1 = typeArray(5,  int)        = int[5]
-  *    iter. 2 -> baseType_2 = typeArray(12, int[5])     = int[5][12]
-  *    iter. 3 -> baseType_3 = typeArray(3,  int[5][12]) = int[5][12][3]
-  */
+  /* Formula : baseType_(i+1) = typeArray( new_dimension , baseType_(i) )
+   * Example : int[5][12][3]
+   *   initial -> baseType_0                             = int
+   *   iter. 1 -> baseType_1 = typeArray(3,  int)        = int[3]
+   *   iter. 2 -> baseType_2 = typeArray(12, int[3])     = int[12][3]
+   *   iter. 3 -> baseType_3 = typeArray(5,  int[12][3]) = int[5][12][3]
+   */
 
- /* Assumptions:
-  *     - t->datatype is not DATATYPE_void 
-  *     - only t->dims[0] could potentially be < 0 (autocomplete first dimension of fpardef)
-  */
+  /* Attention: we iterate the dimensions in reverse order because:
+   *    var a : int[5][12][3] is     array[5] of array[12] of array[3] of int
+   *                          is NOT array[3] of array[12] of array[5] of int
+   *
+   * This is important for the semantic analysis of LMatrix, since:
+   *    typeof(a[i])  IS  (array[12] of array[3] of int)
+   *                  NOT (array[12] of array[5] of int)
+   */
+
+  /* Assumptions:
+   *     - t->datatype is not DATATYPE_void 
+   *     - only t->dims[0] could potentially be < 0
+   *       (this occurs when we autocomplete first dimension of fpardef)
+   */
 
   Type baseType = toType(t->data_type);
   Type tempType;
@@ -49,10 +59,10 @@ Type toType(ParsedType *t)
   /* If no dimensions are given, return the basic type (int or char),
    * otherwise, build the complex type by iterating over the dimensions 
    */
-  for(int n : *(t->dims))
+  for (auto it = t->dims->rbegin(); it != t->dims->rend(); ++it)
   {
-    tempType = typeArray(n, baseType);
-    baseType = tempType;
+    tempType = typeArray(*it, baseType);
+    baseType = tempType;    
   }
 
   return baseType;
