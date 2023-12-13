de227e9534ebb904f4832fc8577c3aa45fe7b71e
Apostolis Stamatis <apostolis.stamatis1@gmail.com>
2023-12-13 10:18:38 +0200

    Adds check for testcases that should produce an error

--- tests/test_syntax.sh
+++ tests/test_syntax.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-# for FILE in /home/apostolis/Shmmy/compilers/grace/ntua_compilers/grace/syntax_gen/progs/*;
-for FILE in /home/gkosm/Documents/ECE/semester_10/compiler_project/yet-another-grace-compiler/tests/type_checking/*;
+for FILE in /home/apostolis/Shmmy/compilers/grace/yet-another-grace-compiler/tests/type_checking/*;
+# for FILE in /home/gkosm/Documents/ECE/semester_10/compiler_project/yet-another-grace-compiler/tests/type_checking/*;
 do 
-    if ! ./gracec < $FILE > /dev/null; then echo "$FILE Failed"
-    # else echo "Success"
+    f="$(basename -- $FILE)"
+    if !([[ $f == err* ]]); then
+        # Testcase should not produce an error
+        if ! ./gracec < $FILE
+            then echo "$f failed: Error not expected but happened"
+        fi
+    # Testcase should produce an error
+    elif ./gracec < $FILE
+        then echo "$f failed: Error expected but did not happen"
     fi
+    # else echo "Success"
 done
