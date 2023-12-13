cafe33baf4ab20376bd47696ee8d7d210e783ace
Apostolis Stamatis <apostolis.stamatis1@gmail.com>
2023-12-14 00:39:56 +0200

    Adds tests

--- tests/test_syntax.sh
+++ tests/test_syntax.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-for FILE in /home/apostolis/Shmmy/compilers/grace/yet-another-grace-compiler/tests/type_checking/*;
+for FILE in /home/apostolis/Shmmy/compilers/grace/yet-another-grace-compiler/tests/scope/*;
 # for FILE in /home/gkosm/Documents/ECE/semester_10/compiler_project/yet-another-grace-compiler/tests/type_checking/*;
 do 
     f="$(basename -- $FILE)"
+    echo "Running $f"
     if !([[ $f == err* ]]); then
         # Testcase should not produce an error
-        if ! ./gracec < $FILE
+        if (! ./gracec < $FILE) &> /dev/null
             then echo "$f failed: Error not expected but happened"
         fi
     # Testcase should produce an error
-    elif ./gracec < $FILE
+    elif (./gracec < $FILE) &> /dev/null
         then echo "$f failed: Error expected but did not happen"
     fi
     # else echo "Success"
